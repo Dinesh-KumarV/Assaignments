04e949fb4431660ebfed8b9892e2ed3ea001566b
User <dineshark33@gmail.com>
2022-11-01 16:50:34 +0530

    assaignment

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 data-ns-test="project-name" description="Zomato clone">
+        Build a Zomato clone
+      </h1>
+    </>
+  );
+};
+
+export default App;
