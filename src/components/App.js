97bca8e23a4f64e0074c47909283fe7ba491f012
User <dineshark33@gmail.com>
2022-11-07 17:10:52 +0530

    changes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
+const Project = ({ name, description }) => {
+  return (
+    <>
+      <h1 data-ns-test="project-name">{name}</h1>
+      <p data-ns-test="project-description">{description}</p>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <>
-      <h1 data-ns-test="project-name" project-description="zomato clone">
-        Zomato clone
-      </h1>
+      <Project name={"Zomato"} description={"To build a Zomato clone"} />
+      <Project name={"Netflix"} description={"To build a Netflix clone"} />
     </>
   );
 };
